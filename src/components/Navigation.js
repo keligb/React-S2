3d3494efa28b44767f3b151e5d8ecb9435914490
Brindeau Kelig <keligbrindeau@gmail.com>
2021-06-18 12:17:21 +0200

    menu déroulant

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,12 +2,20 @@ import React from 'react';
 
 const Navigation = () => {
     return (
-        <ul className="ul-menu">
-            <li className="li-menu"><a className="a-menu" href="/">Accueil</a></li>
-            <li className="li-menu"><a className="a-menu" href="/pays">Pays</a></li>
-            <li className="li-menu"><a className="a-menu" href="/films">Films</a></li>
-            <li className="li-menu"><a className="a-menu" href="/all">All</a></li>
-        </ul>
+        <nav>
+            <ul className="ul-menu">
+                <li className="li-menu"><a className="a-menu" href="/">Accueil</a></li>
+                <li className="li-menu"><a className="a-menu" href="/pays">Pays</a></li>
+                <li className="li-menu deroulant"><a className="a-menu" href="/films">Films</a>
+                    <ul className="sous">
+                        <li><a href="">Les plus populaires</a></li><br />
+                        <li><a href="">Les mieux notés</a></li><br />
+                        <li><a href="">Dernière sortie</a></li><br />
+                    </ul>
+                </li>
+                <li className="li-menu"><a className="a-menu" href="/all">All</a></li>
+            </ul>
+        </nav>
     );
 };
 
